86c9fd909b1dbbebe7763773ec0f033d8790b58c
Tania Nguyen <tanianguyen@Tanias-MBP.austin.rr.com>
2021-01-12 21:22:40 -0600

    completed tables and join csql code

--- Final/avocado_final.sql
+++ Final/avocado_final.sql
@@ -1,41 +1,37 @@
 -- Creating tables for Avocado_finalproject
--- 	 CREATE TABLE Size (
---      	type INT NOT NULL,
---      	small_bags FLOAT NOT NULL,
--- 	 	large_bags FLOAT NOT NULL,
--- 	 	xlarge_bags FLOAT NOT NULL,
--- 	 	total_bags FLOAT NOT NULL,
---    		PRIMARY KEY (type)
--- );
-
--- CREATE TABLE Avocado (
---  	type INT NOT NULL,
--- 	avg_price FLOAT NOT NULL,
---  	date DATE NOT NULL,
---  	year INT NOT NULL,
--- 	geography VARCHAR (20) NOT NULL,
---  	PRIMARY KEY (type, avg_price)
+--  CREATE TABLE size (
+-- 	type VARCHAR (30) NOT NULL,
+--  	small_bags FLOAT NOT NULL,
+-- 	large_bags FLOAT NOT NULL,
+--  	xlarge_bags FLOAT NOT NULL,
+-- 	total_bags FLOAT NOT NULL
 --  );
 
+--    CREATE TABLE avocado (
+--  	type VARCHAR (30) NOT NULL,
+--   	average_price FLOAT NOT NULL,
+-- 	on_date DATE NOT NULL,
+-- 	year INT NOT NULL,
+--   	total_volume FLOAT NOT NULL
+--    );
 
---  CREATE TABLE PLU (
---  	avg_price FLOAT NOT NULL,
---  	plu_4946 FLOAT NOT NULL,
--- 	plu_4225 FLOAT NOT NULL,
---   	plu_4770 FLOAT NOT NULL,
---   	total_volume FLOAT NOT NULL,
---  	PRIMARY KEY (avg_price)
--- );
 
-SELECT * FROM Avocado;
+--    CREATE TABLE plu (
+--  	average_price FLOAT NOT NULL,
+--   	plu_4046 FLOAT NOT NULL,
+--   	plu_4225 FLOAT NOT NULL,
+--  	plu_4770 FLOAT NOT NULL,
+--    	geography VARCHAR (40) NOT NULL
+--  );
+SELECT * FROM plu;
 
--- -- Joining Avocado and PLU
--- SELECT PLU.total_volume,
--- 	 Avocado.type,
---      Avocado.date,
---      Avocado.geography
--- FROM PLU
--- INNER JOIN Avocado
--- ON PLU.avg_price = Avocado.avg_price;
+-- Joining Avocado and PLU
+ SELECT plu.geography,
+		avocado.type,
+   		avocado.on_date,
+    	avocado.total_volume
+ FROM plu
+ INNER JOIN avocado
+ ON plu.average_price = avocado.average_price;
 
--- DROP TABLE Size;
+--  DROP TABLE plu;
